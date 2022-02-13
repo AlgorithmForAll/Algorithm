69e475a046762ef04c44ebd11e05e5bfc8329e93
MSIQOC <43350319+MSIQOC@users.noreply.github.com>
2022-02-13 16:58:29 +0900

    7주차

---
+++ 2022/7주차/[1차] 캐시/신민서.py
@@ -0,0 +1,16 @@
+def solution(cacheSize, cities):
+    answer = 0
+    cache = []
+    for city in cities:
+        temp = city.lower()
+        if temp not in cache:
+            if len(cache) >= cacheSize and cacheSize != 0:
+                cache.pop(0)
+            if cacheSize != 0:
+                cache.append(temp)
+            answer += 5
+        else:
+            answer += 1
+            cache.pop(cache.index(temp))
+            cache.append(temp)
+    return answer
