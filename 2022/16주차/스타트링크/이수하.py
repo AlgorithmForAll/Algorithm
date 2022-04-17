886471d097ba6bee2a12b4d55ee9205bbe794d71
LeeSuHa98 <lapi12@naver.com>
2022-04-17 20:45:19 +0900

    16주차 이수하

---
+++ 2022/16주차/스타트링크/이수하.py
@@ -0,0 +1,26 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+
+def bfs(F, S, G, U, D):
+    queue = deque([[S,0]])
+    visited = {S}
+
+    while queue:
+        floor, count = queue.popleft()
+        
+        if floor == G:
+            return count
+        if floor + U <= F and floor + U not in visited:
+            queue.append([floor + U, count + 1])
+            visited.add(floor + U)
+        if floor - D >= 1 and floor - D not in visited:
+            queue.append([floor - D, count + 1])
+            visited.add(floor - D)
+
+    return "use the stairs"
+
+F, S, G, U, D = map(int, input().split())
+
+print(bfs(F, S, G, U, D))
