101e047fa491cb2c36140d37df1d9359e4fd0b30
Min Seo Shin <forthesouldance@gmail.com>
2022-05-15 22:15:02 +0900

    5월 2주차

--- 신민서/5월 2주차/램프.py
+++ 신민서/5월 2주차/램프.py
@@ -10,16 +10,16 @@ for i in range(N):
 K = int(input())
 cnt = [0]*N
 if K%2: #K가 짝수인 경우
-    for i in range(N):
+    for i in range(N): #매 행마다 0개수 세어주고 똑같은게 몇개 있는지 세어준다.
         zero_cnt = MAPS[i].count(0)
         if zero_cnt%2 and zero_cnt <= K: #0의 개수가 짝수이고 K보다 작거나 같으면 모두 킬 수 있다.
             for j in range(N):
-                if MAPS[i] == MAPS[j]: #예를들어 MAPS[i]가 010이고 MAPS[j]가 010, K가 2인 경우 두개 다 킬 수 있다.
+                if MAPS[i] == MAPS[j]:
                     cnt[i] += 1
 else:
     for i in range(N):
         zero_cnt = MAPS[i].count(0)
-        if not zero_cnt%2 and zero_cnt <= K:
+        if not zero_cnt%2 and zero_cnt <= K: #0의 개수가 홀수이고 K보다 작거나 같으면 모두 킬 수 있다.
             for j in range(N):
                 if MAPS[i] == MAPS[j]:
                     cnt[i] += 1
