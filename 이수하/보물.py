a760e638bea367772a46056aa910d3aa8aba0cb8
LeeSuHa98 <lapi12@naver.com>
2022-05-29 20:10:21 +0900

    5-4

---
+++ 이수하/보물.py
@@ -0,0 +1,18 @@
+'''
+정렬
+'''
+
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+result = 0
+for i in range(N):
+    result += min(A)*max(B)
+    A.pop(A.index(min(A)))
+    B.pop(B.index(max(B)))
+print(result)
